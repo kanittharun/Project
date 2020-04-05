7d4df05df88f5f71653ada5c5f1683da90974bee
Kanittha Rungyaem <61444989+Rungyaem-K@users.noreply.github.com>
2020-04-05 22:59:20 +0700

    Update mqtt_dump.py

--- mqtt_dump.py
+++ mqtt_dump.py
@@ -5,5 +5,6 @@ NETPIE_TOKEN = 'gGXhs95XeBwjjDUyCZzHHS7VGmDNf8xB'
 
 if __name__ == '__main__':
     while True:
-        msg = subscribe.simple('@msg/Fall Detected!/#', hostname='mqtt.netpie.io', port=1883, client_id=CLIENT_ID, auth={'username':NETPIE_TOKEN, 'password':None}, keepalive=10)
-        print("%s %s" % (msg.topic, msg.payload))
+        msg = subscribe.simple('@msg/fallevent/#', hostname='mqtt.netpie.io', port=1883, client_id=CLIENT_ID, auth={'username':NETPIE_TOKEN, 'password':None}, keepalive=10)
+        msg_str = str(msg.payload)
+        print("%s --> %s" % (msg.topic, msg_str))
